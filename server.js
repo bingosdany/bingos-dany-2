27d4cd634eab35253f608e86028076f29704d211
bingosdany <tartaraexpressalvajadas@gmail.com>
2025-05-11 11:59:19 -0500

    Cambiar puerto a 8080 para compatibilidad con Railway

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
